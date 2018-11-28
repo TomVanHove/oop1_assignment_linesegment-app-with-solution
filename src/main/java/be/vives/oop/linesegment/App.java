e706f1b1c1c4c6c54803ca0d8b123446dfe35908
Nico De Witte <nico.dewitte@outlook.com>
2018-11-28 19:13:40 +0100

    Add a small main application to demo the classes

--- src/main/java/be/vives/oop/linesegment/App.java
+++ src/main/java/be/vives/oop/linesegment/App.java
@@ -12,6 +12,19 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      System.out.println("Hello and welcome to the LineSegment demo");
+      System.out.println("Uncomment the code snippets below to test some code");
+
+      // Uncomment to test some code with default constructor
+      // Point p0 = new Point();
+      // System.out.println(p0);
+
+      // Uncomment to test some code with coordinate constructor
+      // Point p1 = new Point(14, 23);
+      // System.out.println(p1);
+
+      // Uncomment to test some code with line segment
+      // LineSegment segment = new LineSegment(2, -13, 0, 44);
+      // System.out.println(segment);
     }
 }
